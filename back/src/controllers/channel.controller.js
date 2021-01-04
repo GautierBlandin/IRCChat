5b292b301842cdbec1e5bc0b184e41317ff3bf56
Blinn Zachari <zacblinn@hotmail.fr>
2021-01-04 22:20:25 +0100

    add function addUser to channel controller

--- back/src/controllers/channel.controller.js
+++ back/src/controllers/channel.controller.js
@@ -4,7 +4,6 @@ const User = require('../models/user.model');
 /**
  * @desc Channel create an empty Channel with current user informations and specified title
  * @route POST /channel/create
- * @body Channel
  */
 exports.channel_create_empty = async (req, res) => {
     try {
@@ -13,22 +12,22 @@ exports.channel_create_empty = async (req, res) => {
 
         const user = await User.findById(currentUserId);
         if (!user) throw "User not found!";
-    
+
         //const channelExist = await Channel.findOne({ title });
         //if (channelExist) throw "Channel with that name already existing!";
-    
+
         const newChannel = new Channel({
             "title": title
         });
-    
+
         newChannel.owner = user;
-    
+
         await newChannel.save();
-    
+
         user.channels.push(newChannel);
-    
+
         await user.save();
-    
+
         res.status(201).json(newChannel);
     } catch (error) {
         res.status(500).json("Error: " + error);
@@ -38,14 +37,13 @@ exports.channel_create_empty = async (req, res) => {
 /**
  * @desc Create Channel with current user at owner and requested user specified in params
  * @route POST /channel/create_withOne/:id
- * @param User 
- * @body Channel
+ * @param {User id} req.params
  */
 exports.channel_create_withOne = async (req, res) => {
     try {
         const currentUserId = req.user.id;
         const requestedUserId = req.params.id;
-        if(currentUserId == requestedUserId) throw "the current user cannot create a channel with himself!";
+        if (currentUserId == requestedUserId) throw "the current user cannot create a channel with himself!";
 
         const currentUser = await User.findById(currentUserId);
         if (!currentUser) throw "Current user or requested user does not exist!";
@@ -85,7 +83,7 @@ exports.channel_create_withOne = async (req, res) => {
 exports.channel_getAll = async (req, res) => {
     try {
         const channel = await Channel.find();
-        if(!channel) throw "There are no channels!";
+        if (!channel) throw "There are no channels!";
 
         res.status(200).json(channel);
     } catch (error) {
@@ -96,13 +94,13 @@ exports.channel_getAll = async (req, res) => {
 /**
  * @desc Channel get specified channel
  * @route GET /channel/getOne/:id
- * @param Channel id
+ * @param {Channel id} req.params
  */
 exports.channel_getOne = async (req, res) => {
     try {
-        const channel = await Channel.findById({_id: req.params.id});
-        if(!channel) throw "channel not found!";
-    
+        const channel = await Channel.findById({ _id: req.params.id });
+        if (!channel) throw "channel not found!";
+
         res.json(channel);
     } catch (error) {
         res.status(404).json("Error: " + error);
@@ -112,15 +110,15 @@ exports.channel_getOne = async (req, res) => {
 /**
  * @desc Update specified channel
  * @route PUT /channel/update/:id
- * @param Channel id
+ * @param {Channel id} req.params
  */
 exports.channel_update = async (req, res) => {
     try {
         const channel = await Channel.findByIdAndUpdate(req.params.id, {
             $set: req.body
         });
-        if(!channel) throw "channel not found!";
-    
+        if (!channel) throw "channel not found!";
+
         res.json(channel);
         console.log('Channel: ' + channel._id + ' updated successfully!');
     } catch (error) {
@@ -131,19 +129,19 @@ exports.channel_update = async (req, res) => {
 /**
  * @desc Delete specified channel
  * @route DELETE /channel/delete/:id
- * @param Channel id
+ * @param {Channel id} req.params
  */
 exports.channel_delete = async (req, res) => {
     try {
         const channel = await Channel.findByIdAndDelete(req.params.id);
-        if(!channel) throw "channel not found!";
+        if (!channel) throw "channel not found!";
 
         const user = await User.findById(channel.owner);
 
         user.channels.pop(channel._id);
 
         await user.save();
-    
+
         res.json(channel);
         console.log('Channel: ' + channel._id + ' deleted successfully!');
     } catch (error) {
@@ -151,8 +149,25 @@ exports.channel_delete = async (req, res) => {
     }
 }
 
-//TODO ADD User to Channel
+/**
+ * @desc Add users to specified channel
+ * @route POST /channel/addUser/:id
+ * @param {Channel id} req.params
+ * @param {Array/String users} req.body
+ */
+exports.channel_addUser = async (req, res) => {
+    try {
+        const channel = await Channel.findByIdAndUpdate(req.params.id, {
+            $push: {
+                users: req.body.users 
+            }
+        });
+        if (!channel) throw "channel not found!";
+
+        res.json(channel);
+    } catch (error) {
+        res.status(404).json("Error: " + error);
+    }
+}
 
 //TODO Kick specified User on Channel
-
-//TODO DELETE Channel
--- back/src/routes/channel.routes.js
+++ back/src/routes/channel.routes.js
@@ -8,5 +8,6 @@ router.post('/create', checkAuth, ChannelController.channel_create_empty); //cre
 router.post('/create_with/:id', checkAuth, ChannelController.channel_create_withOne); 
 router.put('/update/:id', ChannelController.channel_update);
 router.delete('/delete/:id', ChannelController.channel_delete);
+router.put('/addUser/:id', ChannelController.channel_addUser);
 
 module.exports = router;
